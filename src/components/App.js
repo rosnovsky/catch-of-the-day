d7b9d0576ed4120f8a2615455c186f836ad2d751
Artem Rosnovskiy <artem@rosnovsky.us>
2018-03-12 22:23:15 -0700

    Store picker updated

--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,7 @@ class App extends Component {
         return (
             <div className="catch-of-the-day">
                 <div className="menu">
-                    <Header />
+                    <Header tagline="Fresh seafood now"/>
                 </div>
                 <Order />
                 <Inventory />
--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,12 +1,25 @@
 import React, { Component } from "react";
+import { getFunName } from "../helpers";
 
 class StorePicker extends Component {
+    myInput = React.createRef();
+
+    goToStore = (event) => {
+        event.preventDefault(); 
+        console.log(this.myInput)
+    }
+
     render() {
         return (
-            <form className="store-selector">
+            <form className="store-selector" onSubmit={this.goToStore}>
                 <h2>Please Enter a Store</h2>
-                <input type="text" required placeholder="Store name" />
-                <button type="submit">Visit Store →</button>
+                <input 
+                type="text" 
+                ref={this.myInput}
+                required 
+                placeholder="Store name" 
+                defaultValue={getFunName()}/>
+                <button type="submit" >Visit Store →</button>
             </form>
         )}
     }
