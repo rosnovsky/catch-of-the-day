0f4ecb942568ae6791fc6a2d75cb285e15d8909b
Artem Rosnovskiy <artem@rosnovsky.us>
2018-03-15 20:28:46 -0700

    Order state persisting in localStorage

--- src/components/App.js
+++ src/components/App.js
@@ -14,15 +14,25 @@ class App extends Component {
 
     componentDidMount() {
         const { params } = this.props.match;
+        const localStorageRef = localStorage.getItem(params.storeId);
+        if(localStorageRef) {
+            this.setState({ order: JSON.parse(localStorageRef)});
+        };
         this.ref = base.syncState(`${params.storeId}/fishes`, {
             context: this, 
             state: "fishes"
         }); 
     }
+    
+    componentDidUpdate() {
+        const { params } = this.props.match;    
+        localStorage.setItem(params.storeId, JSON.stringify(this.state.order));
+    }
 
     componentWillUnmount() {
         base.removeBinding(this.ref);
     }
+
 
     addFish = fish => {
         const fishes = {...this.state.fishes};
